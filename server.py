a1cce26a9bb6a6a2476d9996818a6b3e5e2c5187
minju <ruri9898@gmail.com>
2023-11-22 15:20:56 +0900

    user info change

--- server.py
+++ server.py
@@ -12,8 +12,8 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 # 데이터 베이스 연결
 def getCon():
-  return pymysql.connect(host="localhost", 
-                     user="root", password="passwd", 
+  return pymysql.connect(host="admin", 
+                     user="root", password="Password", 
                      db="firststep",
                      charset="utf8",
                      cursorclass=pymysql.cursors.DictCursor)
