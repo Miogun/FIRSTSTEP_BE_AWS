ae0ab05b2b92ecba98cd61ee07b37117fea393a7
minju <ruri9898@gmail.com>
2023-11-23 17:32:17 +0900

    codedeploy edit4-pip install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/FIRSTSTEP_BE_AWS
 
-echo ">>> pip install ================="
+echo ">>> sudo apt-get install python3-pip ================="
+sudo add-apt-repository universe 
+sudo apt-get update 
+sudo apt-get install python3-pip
+echo ">>> pip flask ================="
+pip install flask
+
+echo ">>> pip install requirements.txt ================="
 pip install -r requirements.txt
 
 echo ">>>remove template files =============="
@@ -11,4 +18,4 @@ echo ">>> change owner to ubuntu ================"
 chown -R ubuntu /home/ubuntu/FIRSTSTEP_BE_AWS
 
 echo ">>> run app ======================"
-python3 app.py
+flask run --host=0.0.0.0
