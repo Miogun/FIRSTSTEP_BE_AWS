4c94355630891ad73eeaf5468a89fd3a3aa437fb
minju <ruri9898@gmail.com>
2023-11-23 17:39:14 +0900

    codedeploy add run_flask.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/FIRSTSTEP_BE_AWS
 
-echo ">>> sudo apt-get install python3-pip ================="
-sudo add-apt-repository universe 
-sudo apt-get update 
-sudo apt-get install python3-pip
-echo ">>> pip flask ================="
-pip install flask
-
 echo ">>> pip install requirements.txt ================="
 pip install -r requirements.txt
 
@@ -16,6 +9,3 @@ rm -rf appspec.yml requirements.txt
 
 echo ">>> change owner to ubuntu ================"
 chown -R ubuntu /home/ubuntu/FIRSTSTEP_BE_AWS
-
-echo ">>> run app ======================"
-flask run --host=0.0.0.0
